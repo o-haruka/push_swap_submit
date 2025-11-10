30ebbe88421114db6920d421ddb4879fbabfa18b
HarukaOmura <harucha.cha.cha0723@gmail.com>
2025-11-10 16:40:18 +0900

    足りないオペレーション追加

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -28,8 +28,14 @@ void	free_stack(t_node **stack);
 
 // 命令
 void	sa(t_node **a);
+void	sb(t_node **b);
+void	ss(t_node **a, t_node **b);
 void	ra(t_node **a);
+void	rb(t_node **b);
+void	rr(t_node **a, t_node **b);
 void	rra(t_node **a);
+void	rrb(t_node **b);
+void	rrr(t_node **a, t_node **b);
 void	pb(t_node **a, t_node **b);
 void	pa(t_node **a, t_node **b);
 
--- srcs/operations.c
+++ srcs/operations.c
@@ -6,7 +6,7 @@
 /*   By: homura <homura@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 17:52:59 by homura            #+#    #+#             */
-/*   Updated: 2025/11/08 17:53:00 by homura           ###   ########.fr       */
+/*   Updated: 2025/11/10 16:39:28 by homura           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,4 +89,154 @@ void	pa(t_node **a, t_node **b)
 	val = pop_front(b);
 	push_front(a, val);
 	ft_printf("pa\n");
+}
+
+void	sb(t_node **b)
+{
+	t_node	*first;
+	t_node	*second;
+
+	if (!*b || !(*b)->next)
+		return ;
+	first = *b;
+	second = (*b)->next;
+	first->next = second->next;
+	second->next = first;
+	*b = second;
+	ft_printf("sb\n");
+}
+
+void	ss(t_node **a, t_node **b)
+{
+	t_node	*first_a;
+	t_node	*second_a;
+	t_node	*first_b;
+	t_node	*second_b;
+
+	// スタックaの交換
+	if (*a && (*a)->next)
+	{
+		first_a = *a;
+		second_a = (*a)->next;
+		first_a->next = second_a->next;
+		second_a->next = first_a;
+		*a = second_a;
+	}
+	// スタックbの交換
+	if (*b && (*b)->next)
+	{
+		first_b = *b;
+		second_b = (*b)->next;
+		first_b->next = second_b->next;
+		second_b->next = first_b;
+		*b = second_b;
+	}
+	ft_printf("ss\n");
+}
+
+void	rb(t_node **b)
+{
+	t_node	*first;
+	t_node	*cur;
+
+	if (!*b || !(*b)->next)
+		return ;
+	first = *b;
+	cur = *b;
+	while (cur->next)
+		cur = cur->next;
+	*b = first->next;
+	first->next = NULL;
+	cur->next = first;
+	ft_printf("rb\n");
+}
+
+void	rr(t_node **a, t_node **b)
+{
+	t_node	*first_a;
+	t_node	*cur_a;
+	t_node	*first_b;
+	t_node	*cur_b;
+
+	// スタックaのローテート
+	if (*a && (*a)->next)
+	{
+		first_a = *a;
+		cur_a = *a;
+		while (cur_a->next)
+			cur_a = cur_a->next;
+		*a = first_a->next;
+		first_a->next = NULL;
+		cur_a->next = first_a;
+	}
+	// スタックbのローテート
+	if (*b && (*b)->next)
+	{
+		first_b = *b;
+		cur_b = *b;
+		while (cur_b->next)
+			cur_b = cur_b->next;
+		*b = first_b->next;
+		first_b->next = NULL;
+		cur_b->next = first_b;
+	}
+	ft_printf("rr\n");
+}
+
+void	rrb(t_node **b)
+{
+	t_node	*cur;
+	t_node	*prev;
+
+	if (!*b || !(*b)->next)
+		return ;
+	cur = *b;
+	prev = NULL;
+	while (cur->next)
+	{
+		prev = cur;
+		cur = cur->next;
+	}
+	prev->next = NULL;
+	cur->next = *b;
+	*b = cur;
+	ft_printf("rrb\n");
+}
+
+void	rrr(t_node **a, t_node **b)
+{
+	t_node	*cur_a;
+	t_node	*prev_a;
+	t_node	*cur_b;
+	t_node	*prev_b;
+
+	// スタックaのリバースローテート
+	if (*a && (*a)->next)
+	{
+		cur_a = *a;
+		prev_a = NULL;
+		while (cur_a->next)
+		{
+			prev_a = cur_a;
+			cur_a = cur_a->next;
+		}
+		prev_a->next = NULL;
+		cur_a->next = *a;
+		*a = cur_a;
+	}
+	// スタックbのリバースローテート
+	if (*b && (*b)->next)
+	{
+		cur_b = *b;
+		prev_b = NULL;
+		while (cur_b->next)
+		{
+			prev_b = cur_b;
+			cur_b = cur_b->next;
+		}
+		prev_b->next = NULL;
+		cur_b->next = *b;
+		*b = cur_b;
+	}
+	ft_printf("rrr\n");
 }
